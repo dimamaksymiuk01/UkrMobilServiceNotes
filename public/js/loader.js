c0f9fe7c5868c34ffc3507807fd80cb29ac2b247
Dima <admin@admins-MacBook-Air.local>
2023-04-26 17:21:16 +0300

    first commit

---
+++ public/js/loader.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let preloader = document.getElementById('preloader');
+    setTimeout(()=> {
+        preloader.style.display = 'none';
+    }, 1500)
+}
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+import '../scss/loading.scss'
+import './loader.js'
+import '../scss/style.scss'
+import './script.js';
+import './btn.js'
